b08d4458ead4eb5aef686a3d281046252af44937
Nefthaly <64384744+Nefthalyabrego@users.noreply.github.com>
2022-06-28 22:42:38 -0500

    Correcciones en el código del la base de datos

--- AvícolaDB/AvicolaDB.sql
+++ AvícolaDB/AvicolaDB.sql
@@ -5,27 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema AvicolaSystem
+-- Schema AvicolaDB
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema AvicolaSystem
+-- Schema AvicolaDB
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `AvicolaSystem` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
--- -----------------------------------------------------
--- Schema panama
--- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `AvicolaDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
+USE `AvicolaDB` ;
 
 -- -----------------------------------------------------
--- Schema panama
+-- Table `AvicolaDB`.`Avicultores`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `panama` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `AvicolaSystem` ;
-
--- -----------------------------------------------------
--- Table `AvicolaSystem`.`Avicultores`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Avicultores` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Avicultores` (
   `Cedula` VARCHAR(45) NOT NULL,
   `Nombre` VARCHAR(45) NULL,
   `Apellido` VARCHAR(45) NULL,
@@ -35,9 +27,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Fincas`
+-- Table `AvicolaDB`.`Fincas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Fincas` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Fincas` (
   `idFincas` INT NOT NULL AUTO_INCREMENT,
   `Cedula` VARCHAR(45) NULL,
   `Nombre` VARCHAR(45) NULL,
@@ -46,16 +38,16 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Fincas` (
   INDEX `Cedula_idx` (`Cedula` ASC) VISIBLE,
   CONSTRAINT `Cedula`
     FOREIGN KEY (`Cedula`)
-    REFERENCES `AvicolaSystem`.`Avicultores` (`Cedula`)
+    REFERENCES `AvicolaDB`.`Avicultores` (`Cedula`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Medicamentos`
+-- Table `AvicolaDB`.`Medicamentos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Medicamentos` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Medicamentos` (
   `IdMedicamento` INT NOT NULL AUTO_INCREMENT,
   `NombreMedicamento` VARCHAR(45) NULL,
   `CantidadDisponible` VARCHAR(45) NULL,
@@ -64,29 +56,28 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Galpon`
+-- Table `AvicolaDB`.`Galpones`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Galpon` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Galpones` (
   `IdGalpon` INT NOT NULL AUTO_INCREMENT,
   `IdFinca` INT NOT NULL,
   `NumeroGalpon` VARCHAR(45) NOT NULL,
-  `Apellido` VARCHAR(45) NOT NULL,
   `Proposito` VARCHAR(45) NOT NULL,
   `Detalle` VARCHAR(45) NULL,
   PRIMARY KEY (`IdGalpon`),
   INDEX `idFincas_idx` (`IdFinca` ASC) VISIBLE,
   CONSTRAINT `idFincas_idfk`
     FOREIGN KEY (`IdFinca`)
-    REFERENCES `AvicolaSystem`.`Fincas` (`idFincas`)
+    REFERENCES `AvicolaDB`.`Fincas` (`idFincas`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Gallinas`
+-- Table `AvicolaDB`.`Gallinas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Gallinas` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Gallinas` (
   `IdGallina` INT NOT NULL AUTO_INCREMENT,
   `Codigo` INT NOT NULL,
   `IdGalpon` INT NOT NULL,
@@ -97,16 +88,16 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Gallinas` (
   INDEX `IdGalpon_idx` (`IdGalpon` ASC) VISIBLE,
   CONSTRAINT `IdGalpon`
     FOREIGN KEY (`IdGalpon`)
-    REFERENCES `AvicolaSystem`.`Galpon` (`IdGalpon`)
+    REFERENCES `AvicolaDB`.`Galpones` (`IdGalpon`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Gallina_Medicamento`
+-- Table `AvicolaDB`.`Gallina_Medicamento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Gallina_Medicamento` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Gallina_Medicamento` (
   `IdGallina` INT NOT NULL,
   `IdMedicamento` INT NOT NULL AUTO_INCREMENT,
   `FechaAdministracion` VARCHAR(45) NULL,
@@ -114,21 +105,21 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Gallina_Medicamento` (
   INDEX `IdMedicamento_idx` (`IdMedicamento` ASC) VISIBLE,
   CONSTRAINT `IdGallina`
     FOREIGN KEY (`IdGallina`)
-    REFERENCES `AvicolaSystem`.`Gallinas` (`IdGallina`)
+    REFERENCES `AvicolaDB`.`Gallinas` (`IdGallina`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `IdMedicamento`
     FOREIGN KEY (`IdMedicamento`)
-    REFERENCES `AvicolaSystem`.`Medicamentos` (`IdMedicamento`)
+    REFERENCES `AvicolaDB`.`Medicamentos` (`IdMedicamento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Ponederos`
+-- Table `AvicolaDB`.`Ponederos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Ponederos` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Ponederos` (
   `IdPonedero` INT NOT NULL AUTO_INCREMENT,
   `IdGalpon` INT NULL,
   `Fecha` VARCHAR(45) NOT NULL,
@@ -137,16 +128,16 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Ponederos` (
   INDEX `IdGalpon_idx` (`IdGalpon` ASC) VISIBLE,
   CONSTRAINT `IdGalpon_idfk`
     FOREIGN KEY (`IdGalpon`)
-    REFERENCES `AvicolaSystem`.`Galpon` (`IdGalpon`)
+    REFERENCES `AvicolaDB`.`Galpones` (`IdGalpon`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`ActividadPonedero`
+-- Table `AvicolaDB`.`ActividadPonedero`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`ActividadPonedero` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`ActividadPonedero` (
   `FechaActividad` DATETIME NOT NULL,
   `IdGallina` INT NOT NULL,
   `IdGallinaCodigo` INT NOT NULL,
@@ -158,21 +149,21 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`ActividadPonedero` (
   INDEX `IdPonedero_idfk_idx` (`IdPonedero` ASC) VISIBLE,
   CONSTRAINT `IdGallina_idfk`
     FOREIGN KEY (`IdGallina`)
-    REFERENCES `AvicolaSystem`.`Gallinas` (`IdGallina`)
+    REFERENCES `AvicolaDB`.`Gallinas` (`IdGallina`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `IdPonedero_idfk`
     FOREIGN KEY (`IdPonedero`)
-    REFERENCES `AvicolaSystem`.`Ponederos` (`IdPonedero`)
+    REFERENCES `AvicolaDB`.`Ponederos` (`IdPonedero`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Alimento`
+-- Table `AvicolaDB`.`Alimento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Alimento` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Alimento` (
   `IdAlimento` INT NOT NULL AUTO_INCREMENT,
   `NombreAlimento` VARCHAR(45) NOT NULL,
   `CantidadDisponible` VARCHAR(45) NOT NULL,
@@ -181,9 +172,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`GalponAlimento`
+-- Table `AvicolaDB`.`GalponAlimento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`GalponAlimento` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`GalponAlimento` (
   `IdGalpon` INT NOT NULL AUTO_INCREMENT,
   `IdAlimento` INT NOT NULL,
   `Fecha` VARCHAR(45) NOT NULL,
@@ -193,21 +184,21 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`GalponAlimento` (
   INDEX `IdGalpon_idx` (`IdGalpon` ASC) VISIBLE,
   CONSTRAINT `IdAlimento_idfk`
     FOREIGN KEY (`IdAlimento`)
-    REFERENCES `AvicolaSystem`.`Alimento` (`IdAlimento`)
+    REFERENCES `AvicolaDB`.`Alimento` (`IdAlimento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `IdGalponAlimento_idfk`
     FOREIGN KEY (`IdGalpon`)
-    REFERENCES `AvicolaSystem`.`Galpon` (`IdGalpon`)
+    REFERENCES `AvicolaDB`.`Galpones` (`IdGalpon`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Huevos`
+-- Table `AvicolaDB`.`Huevos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Huevos` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Huevos` (
   `IdHuevo` INT NOT NULL AUTO_INCREMENT,
   `IdGallina` INT NOT NULL,
   `Peso` VARCHAR(45) NOT NULL,
@@ -216,16 +207,16 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Huevos` (
   INDEX `IdGallina_idx` (`IdGallina` ASC) VISIBLE,
   CONSTRAINT `IdGallina_idfky`
     FOREIGN KEY (`IdGallina`)
-    REFERENCES `AvicolaSystem`.`ActividadPonedero` (`IdGallina`)
+    REFERENCES `AvicolaDB`.`ActividadPonedero` (`IdGallina`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Productos`
+-- Table `AvicolaDB`.`Productos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Productos` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Productos` (
   `IdProductos` INT NOT NULL AUTO_INCREMENT,
   `NombreProducto` VARCHAR(45) NULL,
   `Imagen` VARCHAR(45) NULL,
@@ -236,9 +227,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Posturas`
+-- Table `AvicolaDB`.`Posturas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Posturas` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Posturas` (
   `Fecha` DATE NOT NULL,
   `IdProducto` INT NOT NULL,
   `IdGalpon` INT NULL,
@@ -248,21 +239,21 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Posturas` (
   PRIMARY KEY (`Fecha`),
   CONSTRAINT `IdGalponPosturas_idfk`
     FOREIGN KEY (`IdGalpon`)
-    REFERENCES `AvicolaSystem`.`Galpon` (`IdGalpon`)
+    REFERENCES `AvicolaDB`.`Galpones` (`IdGalpon`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `IdProducto_idfk`
     FOREIGN KEY (`IdProducto`)
-    REFERENCES `AvicolaSystem`.`Productos` (`IdProductos`)
+    REFERENCES `AvicolaDB`.`Productos` (`IdProductos`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Clientes`
+-- Table `AvicolaDB`.`Clientes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Clientes` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Clientes` (
   `IdClientes` INT NOT NULL AUTO_INCREMENT,
   `NombreCliente` VARCHAR(45) NULL,
   `Direccion` VARCHAR(45) NULL,
@@ -272,9 +263,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `AvicolaSystem`.`Ventas`
+-- Table `AvicolaDB`.`Ventas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Ventas` (
+CREATE TABLE IF NOT EXISTS `AvicolaDB`.`Ventas` (
   `IdCliente` INT NOT NULL AUTO_INCREMENT,
   `IdProducto` INT NULL,
   `FechaVenta` VARCHAR(45) NULL,
@@ -283,73 +274,15 @@ CREATE TABLE IF NOT EXISTS `AvicolaSystem`.`Ventas` (
   INDEX `IdProducto_idx` (`IdProducto` ASC) VISIBLE,
   CONSTRAINT `IdCliente_idfkd`
     FOREIGN KEY (`IdCliente`)
-    REFERENCES `AvicolaSystem`.`Clientes` (`IdClientes`)
+    REFERENCES `AvicolaDB`.`Clientes` (`IdClientes`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `IdProducto_idfdk`
     FOREIGN KEY (`IdProducto`)
-    REFERENCES `AvicolaSystem`.`Productos` (`IdProductos`)
+    REFERENCES `AvicolaDB`.`Productos` (`IdProductos`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-USE `panama` ;
-
--- -----------------------------------------------------
--- Table `panama`.`provincias`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `panama`.`provincias` (
-  `id_provincia` INT NOT NULL DEFAULT '0',
-  `provincia` VARCHAR(20) NOT NULL DEFAULT 'POR DEFINIR',
-  `latitud` VARCHAR(45) NOT NULL,
-  `longitud` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_provincia`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `panama`.`distritos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `panama`.`distritos` (
-  `id_provincia` INT NOT NULL DEFAULT '0',
-  `id_distrito` INT NOT NULL DEFAULT '0',
-  `distrito` VARCHAR(130) NOT NULL DEFAULT 'POR DEFINIR',
-  `latitud` VARCHAR(45) NOT NULL,
-  `longitud` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_distrito`, `id_provincia`),
-  INDEX `ID_PROVINCIA` USING BTREE (`id_provincia`) VISIBLE,
-  CONSTRAINT `distritos_ibfk_1`
-    FOREIGN KEY (`id_provincia`)
-    REFERENCES `panama`.`provincias` (`id_provincia`)
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `panama`.`corregimientos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `panama`.`corregimientos` (
-  `id_provincia` INT NOT NULL DEFAULT '0',
-  `id_distrito` INT NOT NULL DEFAULT '0',
-  `id_corregimiento` INT NOT NULL DEFAULT '0',
-  `corregimiento` VARCHAR(130) NOT NULL DEFAULT 'POR DEFINIR',
-  `latitud` VARCHAR(45) NOT NULL DEFAULT '',
-  `longitud` VARCHAR(45) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_provincia`, `id_distrito`, `id_corregimiento`),
-  INDEX `ID_PROVINCIA` USING BTREE (`id_provincia`) VISIBLE,
-  INDEX `ID_DISTRITO` USING BTREE (`id_distrito`) VISIBLE,
-  CONSTRAINT `corregimientos_ibfk_1`
-    FOREIGN KEY (`id_provincia`)
-    REFERENCES `panama`.`provincias` (`id_provincia`)
-    ON UPDATE CASCADE,
-  CONSTRAINT `corregimientos_ibfk_2`
-    FOREIGN KEY (`id_distrito`)
-    REFERENCES `panama`.`distritos` (`id_distrito`)
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
