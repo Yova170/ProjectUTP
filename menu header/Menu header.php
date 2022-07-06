26c51ffc170883290ef1ea642e1c8aa272fcbf38
espinosa03 <104846201+espinosa03@users.noreply.github.com>
2022-07-06 15:11:38 -0500

    menu

---
+++ menu header/Menu header.php
@@ -0,0 +1,23 @@
+<header id="header">
+        <div class="container__header">
+            <div class="logo">
+                <img src="images/logo-magtimus.png" alt="">
+            </div>
+            <div class="container__nav">
+                <nav id="nav">
+                    <ul class="menu">
+                        <li><a href="login.php" class="select">INICIO</a></li>
+                        <li><a href="#">INVENTARIO</a></li>
+                        <li><a href="#">COMPRAS</a></li>
+                        <li><a href="#">PROVEEDORES</a>
+                           
+                        </li>
+                           
+                        <li><a href="#">ACTIVIDADES</a></li>
+                    </ul>
+                </nav>
+                <div class="btn__menu" id="btn_menu"><i cl ass="fas fa-bars"></i></div>
+            </div>
+        </div>
+        <link rel="stylesheet" href= "style\css\style.css">
+    </header>
