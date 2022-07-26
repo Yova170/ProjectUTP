f064f9edea465cebc1b6d4d90fe43bc411662e41
David <Dominus_luporum@bitbucket.org>
2022-07-25 21:21:52 -0500

    conexión

--- compra_venta/global/config.php
+++ compra_venta/global/config.php
@@ -9,4 +9,17 @@ define("USUARIO","root");
 define("PASSWORD","");
 define("BD","gallinero");
 
+$servidor="mysql:dbname=".BD.";host=".SERVIDOR;
+
+    try
+    {
+        $pdo= new PDO($servidor,USUARIO,PASSWORD,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND=> "SET NAMES utf8")
+        );
+       // echo "<script> alert('conectados...')</script>";
+
+    }catch(PDOException $e){
+     //   echo "<script> alert('Error...')</script>";
+    }
+
 ?>
